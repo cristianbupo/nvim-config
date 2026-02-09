aa245192ecbcba5ee6263a352ec6fb485ccb5b7b
Cristian Bustamante <crbustamantep@unal.edu.co>
2026-02-09 12:17:10 +0100

    add ruoes for extra files, make copilot ignore some files

---
+++ after/ftplugin/cpp.lua
@@ -0,0 +1,4 @@
+-- ~/.config/nvim/after/ftplugin/cpp.lua
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.expandtab = true
--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,12 +3,19 @@ return {
   {
     'zbirenbaum/copilot.lua',
     cmd = 'Copilot',
-    event = 'InsertEnter',
+    event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       require('copilot').setup {
+        filetypes = {
+          tex = false, -- Disable for LaTeX files
+          plaintex = false, -- Disable for plain text files
+          md = false, -- Disable for Markdown files
+          ['*'] = true, -- Enable for all file types
+        },
         suggestion = {
           enabled = true,
           auto_trigger = true,
+          copilot_model = 'gpt-4o-copilot',
           keymap = {
             accept = '<C-l>', -- Ctrl+l to accept
             accept_word = '<M-w>', -- Alt+w to accept word
@@ -29,14 +36,23 @@ return {
     },
     build = 'make tiktoken',
     opts = {
+      model = 'gpt-5.2',
       window = {
         layout = 'vertical',
         width = 0.3,
       },
+      -- Allow the chat to access the context of all open buffers
+      contexts = {
+        buffer = { enabled = true },
+        file = { enabled = true },
+      },
     },
     keys = {
       { '<leader>cc', ':CopilotChatToggle<CR>', desc = 'Toggle Copilot Chat Sidebar' },
+      { '<leader>co', '<cmd>CopilotChatOpen<cr>', desc = 'Open Chat' },
+      { '<leader>cx', '<cmd>CopilotChatClose<cr>', desc = 'Close Chat' },
       { '<leader>ce', ':CopilotChatExplain<CR>', desc = 'Explain Code' },
+      { '<leader>cr', ':CopilotChatReset<CR>', desc = 'Reset Chat' },
     },
   },
 }
